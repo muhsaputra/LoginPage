cc69a696f56708142406159ef25f3f58183b6e39
muhsaputra <muhsaputrabiz@gmail.com>
2023-06-29 19:01:25 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', 'index-login.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins'],
+      },
+      colors: {
+        primary: '#14b8a6'
+      }
+    },
+  },
+  plugins: [],
+}
